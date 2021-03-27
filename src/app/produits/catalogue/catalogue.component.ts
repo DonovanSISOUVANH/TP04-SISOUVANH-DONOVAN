34a62fadb0fa60b5fd150fde47bee1d1205e4b66
Donovan <donogf@hotmail.fr>
2021-03-27 23:50:58 +0100

    shared

--- src/app/produits/catalogue/catalogue.component.ts
+++ src/app/produits/catalogue/catalogue.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngxs/store';
+import {AddReference} from '../../../shared/actions/panier.action';
+import {Reference} from '../../../shared/models/reference';
+import { PanierState } from 'src/shared/states/panier-state';
 
 @Component({
   selector: 'app-catalogue',
@@ -7,9 +11,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CatalogueComponent implements OnInit {
 
-  constructor() { }
+  constructor(private store : Store) { }
 
   ngOnInit(): void {
+  }
+
+  addPanier (ref : string) {
+    console.log (ref);
+    
+    this.store.dispatch (new AddReference ({"reference":ref}));
   }
 
 }
