ea9339bd057993ce681f5987725223a8aeb7737a
Donovan <donogf@hotmail.fr>
2021-03-27 23:48:16 +0100

    Next

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AccueilComponent } from './accueil/accueil.component';
+import { CompteComponent } from './compte/compte.component';
+import { ProduitsComponent } from './produits/produits.component';
+import { CatalogueComponent } from './produits/catalogue/catalogue.component';
+import { AuthGuard } from './auth.guard';
+import { NgxsModule } from '@ngxs/store';
+import { PanierState } from '../shared/states/panier-state'
 
 const appRoutes : Routes = [
   {path:'',component:AccueilComponent},
+  {path:'compte',component:CompteComponent},
+  {path:'produits/catalogue',component:CatalogueComponent},
   {path: 'produits', canActivate: [AuthGuard], loadChildren: () => import('./produits/produits.module').then(m => m.ProduitsModule)}
 ]
 
@@ -14,11 +22,15 @@ const appRoutes : Routes = [
   declarations: [
     AppComponent,
     HeaderComponent,
-    AccueilComponent
+    AccueilComponent,
+    CompteComponent,
+    ProduitsComponent,
+    CatalogueComponent,
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    NgxsModule.forRoot ([PanierState])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngxs/store';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private store : Store) { }
 
   ngOnInit(): void {
   }
