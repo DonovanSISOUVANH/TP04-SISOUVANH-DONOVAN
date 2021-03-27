cc6603e95ced1232e7d61d4f956cf94215470543
Donovan <donogf@hotmail.fr>
2021-03-28 00:15:39 +0100

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,7 +25,7 @@ const appRoutes : Routes = [
     AccueilComponent,
     CompteComponent,
     ProduitsComponent,
-    //CatalogueComponent,
+    CatalogueComponent,
   ],
   imports: [
     BrowserModule,
