e45c878aa2320d70709aeff4452f1ef7f146b445
Donovan <donogf@hotmail.fr>
2021-04-15 10:47:23 +0200

    Import formsmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { CompteComponent } from './compte/compte.component';
